960bc03d0baa723e3dafa34d70d75f087b12acb5
Zach Sim <simjza@gmail.com>
2017-11-19 21:48:54 +1300

    Add react_router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter, Route } from 'react-router';
 import './index.css';
 import MainMenu from './components/MainMenu';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MainMenu />, document.getElementById('root'));
+const Routes = () =>
+  Object.entries({
+    main_menu: MainMenu
+  }).map(([key, component]) => <Route {...{ key, path: key, component }} />);
+
+ReactDOM.render(
+  <MemoryRouter initialEntries={['main_menu']}>
+    <Routes />
+  </MemoryRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
